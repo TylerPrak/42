20250ecdd602171168835bef199fb6cdbe286c48
Tyler PRAK <prayforparasites@gmail.com>
2017-06-10 17:55:41 -0700

    Added ft_atoi.c

---
+++ Libft/srcs/ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoii.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tprak <tprak@student.42.us.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/10 17:15:58 by tprak             #+#    #+#             */
+/*   Updated: 2017/06/10 17:53:55 by tprak            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+int		ft_atoi(const char *str)
+{
+	int num;
+	int negcheck;
+
+	num = 0;
+	negcheck = 1;
+	while (*str == '\t' || *str == '\v' || *str == '\f'
+		|| *str == '\r' || *str == '\n' || *str == ' ')
+		str++;
+	if (*str == '-')
+		negcheck = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		num *= 10;
+		num += *str - '0';
+		str++;
+	}
+	return (num *= negcheck);
+}
