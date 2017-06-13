74f5bcdce6c5cd8bbb1d2bdf2cadc2ba3ccc9293
Tyler PRAK <prayforparasites@gmail.com>
2017-06-13 03:58:17 -0700

    Fixed a case where incrementing i inside the while condition would return a pointer to the wrong byte

--- Libft/srcs/ft_memccpy.c
+++ Libft/srcs/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: tprak <tprak@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/11 23:03:21 by tprak             #+#    #+#             */
-/*   Updated: 2017/06/11 23:05:41 by tprak            ###   ########.fr       */
+/*   Updated: 2017/06/13 03:52:18 by tprak            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	unsigned char *s1;
-	unsigned char *s2;
-	size_t i;
+	unsigned char	*s1;
+	unsigned char	*s2;
+	size_t			i;
 
 	s1 = (unsigned char*)dst;
 	s2 = (unsigned char*)src;
 	i = 0;
-
-	while (i++ < n)
+	while (i < n)
 	{
 		if (*s2 == (unsigned char)c)
 			return (dst + i + 1);
 		*s1++ = *s2++;
+		i++;
 	}
 	return (NULL);
 }
