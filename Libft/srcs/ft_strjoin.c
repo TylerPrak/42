61442a97a592bb7fd4b2480216b631657f726d55
Tyler PRAK <prayforparasites@gmail.com>
2017-06-15 23:17:25 -0700

    Added ft_strjoin.c

---
+++ Libft/srcs/ft_strjoin.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tprak <tprak@student.42.us.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/15 23:15:03 by tprak             #+#    #+#             */
+/*   Updated: 2017/06/15 23:17:11 by tprak            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = (char*)malloc(sizeof(s1) + sizeof(s2));
+	if (!s1 || !s2 || !str)
+		return (NULL);
+	while (*s1 != '\0')
+	{
+		str[i] = *s1;
+		s1++;
+		i++;
+	}
+	while (*s2 != '\0')
+	{
+		str[i] = *s2;
+		s2++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
